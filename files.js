098780fde24ac0a008b8fa9b06c8e733fa833207
namnguyen7133-png <namnguyen7133@gmail.com>
2026-01-21 16:38:59 +0700

    Add initial file list to files.js

---
+++ files.js
@@ -0,0 +1,9 @@
+const FILE_LIST = [
+  "001-tim-ten-nguoi.html",
+  "002-thoi-gian-bieu-lam-viec.html",
+  "003-shopee-tu-dong-lam-hh.html",
+  "004-shopee-tim-chu-cuoi-doan-van.html",
+  "005-shopee-tim-san-pham-trang-tri-noi-that.html",
+  "006-file-cua-ban.html",
+  "007-file-cua-ban.html"
+];
